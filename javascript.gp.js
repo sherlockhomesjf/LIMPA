3646d1985d28a80a4bf170be24631a034094c555
sherlockhomesjf <134359476+sherlockhomesjf@users.noreply.github.com>
2023-07-22 23:51:51 -0300

    Delete javascript.gp.js

--- javascript.gp.js
+++
@@ -1,5 +0,0 @@
-{
-  "geoplugin_status":403,
-  "geoplugin_message": "Unauthorised: a valid access key is required to access the geoplugin API over SSL",
-  "geoplugin_url": "https://www.geoplugin.com/premium#ssl_access_per_year"
-}
